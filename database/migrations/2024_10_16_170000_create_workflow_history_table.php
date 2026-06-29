8ee0ecdab13451de7872321919db1d0b49e16587
Masaki Kawabata Neto <kawabata@usp.br>
2026-06-29 13:26:10 -0300

    refactor migrations

---
+++ database/migrations/2024_10_16_170000_create_workflow_history_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Executa a migração para criar a tabela workflow_history.
+     */
+    public function up(): void
+    {
+        Schema::create('workflow_history', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('workflow_object_id')->constrained()->onDelete('cascade');
+            $table->string('transition'); // nome da transiçao que gerouo histórico
+            $table->string('from_places'); // Estado(s) de origem
+            $table->string('to_places');   // Estado(s) de destino
+            $table->foreignId('user_id')->constrained('users'); // Usuário que realizou a ação
+            $table->unsignedBigInteger('form_submission_id')->nullable(); // ID da submissão do formulário (opcional)
+            $table->json('metadata')->nullable(); // Metadados adicionais em formato JSON
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverte a migração.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('workflow_history');
+    }
+};
