b7699583828ee54ecbbdf01ef4d0cf3f03da9e09
ViniReiwz <vinireisg@usp.br>
2026-07-09 13:03:20 -0300

    Modificações do WorkflowHistory
    Adicionados os campos do histórico à protected $fillable = [...], e fazendo os devidos casts (de json para array)

--- src/Models/WorkflowHistory.php
+++ src/Models/WorkflowHistory.php
@@ -10,8 +10,20 @@ use Uspdev\Forms\Models\FormSubmission;
 class WorkflowHistory extends Model
 {
     protected $table = 'workflow_history';
+
+    protected $fillable = [
+        'workflow_object_id',
+        'transition_name',
+        'user_id',
+        'metadata',
+        'form_submission_id',
+        'from_places',
+        'to_places',
+    ];
     protected $casts = [
         'metadata' => 'array',
+        'from_places' => 'array',
+        'to_places' => 'array',
     ];
 
     public function workflowObject(): BelongsTo
